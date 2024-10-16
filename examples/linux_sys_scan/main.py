32c610b17fc897f6467bbd31812112e50fb8e6e5
Viktor Kazanin <vkazanin@kaaiot.io>
2024-10-16 15:48:19 +0300

    AGILE-5592: include 'mqtt' in DEFAULT_KPC_HOST string

--- examples/linux_sys_scan/main.py
+++ examples/linux_sys_scan/main.py
@@ -101,7 +101,7 @@ def run_endpoint(kpc_host, kpc_port, app_version, token, metadata, update_interv
         sys_scan_client.step()
 
 def main():
-    kaa_kpc_host = os.environ.get("DEFAULT_KPC_HOST")
+    kaa_kpc_host = f"mqtt.{os.environ.get('DEFAULT_KPC_HOST')}"
     kaa_kpc_port = int(os.environ.get("DEFAULT_KPC_PORT", '1883'))
     app_version = os.environ.get("APPLICATION_VERSION")
     token = os.environ.get("ENDPOINT_TOKEN")
