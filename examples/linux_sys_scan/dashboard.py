42998480e86cfa0f516a0d6915aab0e36f73a35b
Viktor Kazanin <vkazanin@kaaiot.io>
2024-10-18 15:21:35 +0300

    AGILE-5592: dashboard generator update

--- examples/linux_sys_scan/dashboard.py
+++ examples/linux_sys_scan/dashboard.py
@@ -1,7 +1,12 @@
 import json
+import os
 from constants import app_version, app_name, tenant_id
 
-json_file_paths = ['./dashboards/home.json', './dashboards/device_overview.json', './test.json']
+template_folder = './dashboards/templates/'
+output_folder = './dashboards/output/'
+
+json_file_paths = [os.path.join(template_folder, file) for file in os.listdir(template_folder) if file.endswith('.json')]
+os.makedirs(output_folder, exist_ok=True)
 
 def initialize_dashboards():
     def replace_placeholders(obj):
@@ -10,7 +15,7 @@ def initialize_dashboards():
                 if isinstance(value, str):
                     obj[key] = value.replace('{APP_NAME}', app_name).replace('{APP_VERSION}', app_version).replace('{TENANT_ID}', tenant_id)
                 else:
-                    replace_placeholders(value) 
+                    replace_placeholders(value)
         elif isinstance(obj, list):
             for item in obj:
                 replace_placeholders(item)
@@ -25,9 +30,10 @@ def initialize_dashboards():
             else:
                 print(f"Warning: {json_file_path} does not contain a JSON object.")
 
-            with open(json_file_path, 'w') as file:
+            output_file_path = os.path.join(output_folder, os.path.basename(json_file_path))
+            with open(output_file_path, 'w') as file:
                 json.dump(data, file, indent=4)
 
-            print(f"Placeholders replaced successfully in {json_file_path}.")
+            print(f"Placeholders replaced successfully in {output_file_path}.")
         except Exception as e:
             print(f"ERROR: An error occurred while processing {json_file_path}. {e}")
