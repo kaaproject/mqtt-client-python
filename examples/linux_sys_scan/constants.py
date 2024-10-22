54833022936541bda38acfc2f2355153e40271d9
Viktor Kazanin <vkazanin@kaaiot.io>
2024-10-22 18:10:49 +0300

    AGILE-5592: add default value to DEFAULT_KPC_HOST to prevent crash

--- examples/linux_sys_scan/constants.py
+++ examples/linux_sys_scan/constants.py
@@ -3,7 +3,7 @@ import os
 
 logger = logging.getLogger(__name__)
 
-kaa_kpc_host = f"mqtt.{os.environ.get('DEFAULT_KPC_HOST')}"
+kaa_kpc_host = f"mqtt.{os.environ.get('DEFAULT_KPC_HOST') or 'cloud.kaaiot.com'}"
 kaa_kpc_port = int(os.environ.get("DEFAULT_KPC_PORT", '1883'))
 app_version = os.environ.get("APPLICATION_VERSION")
 app_name = os.environ.get("APPLICATION_NAME")
