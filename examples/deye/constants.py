45077d29522ace49fbde0bddc2dd3cfaeb857551
viktorkw <viktorkw1201@gmail.com>
2024-11-05 16:24:22 +0200

    AGILE-5724: updated constants for deye enviroment

--- examples/deye/constants.py
+++ examples/deye/constants.py
@@ -1,6 +1,9 @@
 import os
 
 
+"""[REQUIRED] For communications with Kaa platform"""
+ip = os.environ.get('IP')
+serial_number = int(os.environ.get('SERIAL_NUMBER'))
 kaa_kpc_host = os.environ.get('DEFAULT_KPC_HOST') or "mqtt.cloud.kaaiot.com"
 if kaa_kpc_host.startswith("mqtt://"):
     kaa_kpc_host = kaa_kpc_host[len("mqtt://"):]
@@ -9,5 +12,7 @@ kaa_kpc_port = int(os.environ.get("DEFAULT_KPC_PORT", '1883'))
 app_version = os.environ.get("APPLICATION_VERSION")
 token = os.environ.get("ENDPOINT_TOKEN")
 
-ip = os.environ.get('IP')
-serial_number = int(os.environ.get('SERIAL_NUMBER'))
+
+"""[OPTIONAL] For dashboard templates"""
+app_name = os.environ.get("APPLICATION_NAME")
+tenant_id = os.environ.get("TENANT_ID")
