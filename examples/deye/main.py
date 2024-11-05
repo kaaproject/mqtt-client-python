d99ce2ac8b992634cc3e6a24bdc27ee97fe23877
viktorkw <viktorkw1201@gmail.com>
2024-11-05 10:36:35 +0200

    AGILE-5724: add command handlers

--- examples/deye/main.py
+++ examples/deye/main.py
@@ -1,11 +1,11 @@
 import logging
 import time
-import uuid
 import datetime
 
-from constants import kaa_kpc_host, kaa_kpc_port, token, app_version
-from kaa_mqtt_client import KaaMqttClient
+from constants import kaa_kpc_host, kaa_kpc_port, token, app_version, ip, serial_number
+from kaa_mqtt_client import KaaMqttClient, Command, CommandResponse
 from inverter import get_metadata, get_timestamp
+from pysolarmanv5 import PySolarmanV5
 
 
 class DeyeClient:
@@ -14,6 +14,7 @@ class DeyeClient:
         self.metadata = metadata
         self.update_interval = update_interval
         self.is_running = True
+        self.define_handlers()
 
 
     @property
@@ -27,11 +28,73 @@ class DeyeClient:
             raise Exception("Not supported metadata format")
         if not hasattr(self, "_metadata"):
             self._metadata = metadata
-            self._metadata["serial"] = str(uuid.uuid4())
         else:
             self._metadata.update(**metadata)
         self._metadata["lastMaintenance"] = str(datetime.datetime.now())
         self.publish_metadata()
+
+
+    def define_handlers(self):
+        @self.kaa_client.command_handler("deye_write")
+        def execute_deye_command(command: Command) -> CommandResponse:
+            register_list = command.payload['settings']
+            print(register_list)
+            response_object = {}
+            pysolarman_instance = PySolarmanV5(address=ip, serial=serial_number)
+
+            for register_item in register_list:
+                register_id = int(register_item["register"])
+                new_register_value = int(register_item["value"])
+                amount_of_values_to_read = 1
+                initial_register_value = None
+                updated_register_value = None
+                
+                try:
+                    initial_register_value = pysolarman_instance.read_holding_register_formatted(register_id, amount_of_values_to_read)
+                    pysolarman_instance.write_multiple_holding_registers(register_id, [new_register_value])
+                    updated_register_value = pysolarman_instance.read_holding_register_formatted(register_id, amount_of_values_to_read)
+                    
+                    if initial_register_value is None or updated_register_value is None or new_register_value != int(updated_register_value):
+                        raise Exception("Client could not write the required value.")
+                    
+                    response_message = f"Success: Value '{new_register_value}' written to register '{register_id}'."
+                    response_object[register_id] = response_message
+                    print(response_message)
+                except Exception as e:
+                    pysolarman_instance.write_multiple_holding_registers(register_id, [initial_register_value])
+                    response_message = f"Error: Value '{new_register_value}' for register '{register_id}' is invalid. Skipping... \nReason: {str(e)}"
+                    response_object[register_id] = response_message
+                    print(response_message)
+                    continue
+            
+            return CommandResponse(command=command, status_code=200, reason_phrase="DONE", payload=response_object)
+        
+        @self.kaa_client.command_handler("deye_read")
+        def read_deye_value(command: Command) -> CommandResponse:
+            register_list = command.payload['settings']
+            pysolarman_instance = PySolarmanV5(address=ip, serial=serial_number)
+            response_object = {}
+
+            for register_item in register_list:
+                register_id = int(register_item["register"])
+                amount_of_values_to_read = 1
+                    
+                try:
+                    register_value = pysolarman_instance.read_holding_register_formatted(register_id, amount_of_values_to_read)
+                    
+                    if register_value is None:
+                        raise Exception("Client could not read the required register.")
+                    
+                    response_message = f"Success: Value of register_id '{register_id}' is '{register_value}'"
+                    response_object[register_id] = response_message
+                    print(response_message)
+                except Exception as e:
+                    response_message = f"Error: register_id of '{register_id}' is invalid. Skipping... \nReason: {str(e)}"
+                    response_object[register_id] = response_message
+                    print(response_message)
+                    continue
+                
+            return CommandResponse(command=command, status_code=200, reason_phrase="DONE", payload=response_object)
 
 
     def publish_metadata(self):
